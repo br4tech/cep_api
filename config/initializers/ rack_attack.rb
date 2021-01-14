b0a96cf869fe80a07a13e273ac4399180b10d665
Guilherme Silva <Guilherme.hsilva@ao3tech.com>
2021-01-14 08:42:12 -0300

    add rack-attack in app

---
+++ config/initializers/ rack_attack.rb
@@ -0,0 +1,12 @@
+class Rack::Attack
+ Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+  safelist('allow-localhost') do |req|
+    '127.0.0.1' == req.ip || '::1' == req.ip
+  end
+
+ # Allow an IP address to make 10 requests every 10 seconds
+  throttle('req/ip', limit: 5, period: 5) do |re|
+   req.ip
+  end
+end
